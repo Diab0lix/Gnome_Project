4f7d7505a767a63e81bd0665503537f58ca08500
thomas.blt <thomas.blot1997@gmail.com>
2019-11-21 11:09:23 +0100

    [Client.java] merge with the rent sale and buy functions

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class Client {
 
     private String name;
-
     private String surname;
     protected Stock stock;
     protected double wallet;
@@ -28,18 +27,27 @@ public class Client {
 
     public void Hunt() {
         // TODO implement here
+        System.out.println("A client can t hunt");
     }
 
     public void Buy() {
         // TODO implement here
+        Normal thomas = new Normal();
+        this.wallet = this.wallet - thomas.getPrice();
+        stock.addGnome(thomas);
     }
 
     public void Rent() {
         // TODO implement here
+        Normal thomas = new Normal();
+        thomas.getPrice();
+        stock.listRentedGnome.add(thomas);
     }
 
-    public void Sale() {
+    public void Sale(Gnome gnome) {
         // TODO implement here
+        wallet += gnome.getPrice() ;
+        stock.delGnome(gnome);
     }
 
     public String getStock() {
