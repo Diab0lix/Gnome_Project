e995af5faaf70ee2068cdcd7d30a4c946b4c497f
Thierry Frycia <thierry@frycia.be>
2019-11-21 10:36:33 +0100

    [Client.java] add wallet and funds management

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -6,14 +6,24 @@ public class Client {
 
     private String surname;
     protected Stock stock;
+    protected double wallet;
 
     /**
      * Default constructor
      */
-    public Client(String name, String surname) {
+    public Client(String name, String surname, double wallet) {
         this.name = name;
         this.surname = surname;
+        this.wallet =  wallet;
         stock = Stock.getInstance();
+    }
+
+    public void AddFunds(double money) {
+        this.wallet += money;
+    }
+
+    public GetFunds() {
+        return wallet;
     }
 
     public void Hunt() {
