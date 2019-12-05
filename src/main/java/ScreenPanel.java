685ec255a975ebee7b8381dcc73d905f032b940d
thomas.blt <thomas.blot1997@gmail.com>
2019-12-05 11:48:04 +0100

    [Resources.*] push of png and update of Screenpanel

--- src/main/java/ScreenPanel.java
+++ src/main/java/ScreenPanel.java
@@ -4,5 +4,9 @@ public class ScreenPanel extends JPanel {
     ScreenPanel(){
         setBackground(Color.BLACK);
         setSize(500,465);
+
+        ImageIcon Sans = new ImageIcon("src/Resources/dwarf.png");
+        JLabel sanslab = new JLabel(Sans);
+        add(sanslab);
     }
 }
